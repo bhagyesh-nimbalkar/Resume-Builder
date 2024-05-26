1744d591ce07b7dd19755802393d359b57273e1b
Bhagyesh Nimbalkar <143781595+bhagyesh-nimbalkar@users.noreply.github.com>
2024-05-26 13:44:44 +0530

    Update config.php

--- dnlib/config.php
+++ dnlib/config.php
@@ -1,11 +1,11 @@
 <?php
   // Database Connection 
-  const DB_NAME='cv';
-  const DB_HOST="localhost";
-  const DB_USER="root";
-  const DB_PASS="";
+  const DB_NAME='defaultdb';
+  const DB_HOST="mysql-1f772a1c-nimbalkarbhagyesh6979-2c7a.h.aivencloud.com";
+  const DB_USER="avnadmin";
+  const DB_PASS="AVNS_5fMdIiCTmX4GNtkDMze";
 
   // URL 
-  const SITE_URL = 'http://localhost/Resume%20Builder/';
+  const SITE_URL = 'https://resume-builder-application-using-php.vercel.app/';
   const ASSET_URL = SITE_URL.'assets/';
 ?>
